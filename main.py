7cf5c36c223a4cfad6e887497d42dbd975f12788
nwihardjo <nwihardjo@connect.ust.hk>
2017-08-03 18:17:04 +0800

    update probablepeople API

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 import csv
 import xlrd
+import requests
+import json
 
 COMPANY_IDENTIFIERS = []
+API_KEY = 'ac5e0a11-8406-4b94-a57f-b1a6661b0886'
+URL = 'https://parserator.datamade.us/api/probablepeople/'
+
+def nameAnalysis(name):
+	# analyse the name passed using probablepeople API
+	# because probablepeople library can't be easily installed in our machine
+	# return True if it is a company, return False otherwise
+	params = {'api_key': API_KEY, 'name': name, 'fmt' = 'json'}
+
+	response = requests.get(URL, params = params)
+	if response.status_code != 200:
+		print('API call failed. Check API call in nameAnalysis function')
+	
+	return (response.text.json()['type'] == 'Corporation')
 
 class investor:
 	def __init__(self, id, name, inputType):
 		self.id = id
-		self.name = name.lower()
+		self.name = name.title()
 		self.inputType = inputType
+		self.type = nameAnalysis(name)
 
 	def compare(comparedInvestorName):
 		# compare the investor's name with the existing ones
-		# return False if compared investors are the same
+		# return True if compared investors are the same
+
+		for subname in comparedInvestorName.split():
+			if subname in self.name:
+				return False
+
+
+
+
 
 
 def main():
@@ -24,12 +49,12 @@ def main():
 		for row in reader:
 			for singleInvestor in investors:
 				if type(singleInvestor) is not list:
-					if not singleInvestor.compare(row['name'].lower()):
+					if singleInvestor.compare(row['name'].lower()):
 						investors[index(singleInvestor)] = [singleInvestor, investor(row['id'], row['name'], row['input'])]
 					else:
 						investors.append(investor(row['id'], row['name'], row['input']))
 				else if type(singleInvestor) is list:
-					if not singleInvestor[0].compare(row['name'].lower()):
+					if singleInvestor[0].compare(row['name'].lower()):
 						investors[index(singleInvestor)].append(investor(row['id'], row['name'], row['input']))
 					else:
 						investors.append(investor(row['id'], row['name'], row['input']))
