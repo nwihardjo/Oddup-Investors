c6c697b25b2c95b93e5a274397389aed1ee39be8
nwihardjo <nwihardjo@connect.ust.hk>
2017-08-07 18:05:53 +0800

    improve compare and analysis algo

--- main.py
+++ main.py
@@ -1,68 +1,96 @@
 import csv
 import requests
 import json
+import re
 
-COMPANY_IDENTIFIERS = []
+REJECTED_SPECIAL_IDENTIFIERS = ['Zhuhai', 'Yunnan', 'York', 'Wu','William']
+APPROVED_SPECIAL_IDENTIFIERS = ['Xiaomi']
 API_KEY = 'ac5e0a11-8406-4b94-a57f-b1a6661b0886'
 URL = 'https://parserator.datamade.us/api/probablepeople/'
 
 def nameAnalysis(name):
-	# analyse the name passed using probablepeople API
+	"""analyse the name passed using probablepeople API and hardcode"""
 	# because probablepeople library can't be easily installed in our machine
 	# return True if it is a company, return False otherwise
-	
+
+
+	# for investor name which has non-alphanumeric case
+	# exclude the characters in brackets because it will automatically recognised as Corporation
+	name = ' '.join(name.split('(')[0])
+
 	params = {'api_key': API_KEY, 'name': name, 'fmt': 'json'}
-
 	response = requests.get(URL, params = params)
+
 	if response.status_code != 200:
-		print('API call failed. Check API call in nameAnalysis function')
+		print('DEBUG: API call failed for {}. Check API call in nameAnalysis function'.format(name))
+
+	if name in APPROVED_SPECIAL_IDENTIFIERS:
+		return True
+
+	try:
+		return (response.json()['type'] == 'Corporation')
+	except:
+		return False
+
+class Investor:
 	
-	return (response.text.json()['type'] == 'Corporation')
+	def debugInvestor(self):
+		if self.type == True: x = 'Corporation'
+		else: x = 'Person' 
+		
+		if self.rootName:
+			print("DEBUG: CREATING & ADDING {} NAMED {}, ROOT: {}".format(x, self.name, self.rootName))
+		else:
+			print("DEBUG: CREATING & ADDING {} NAMED {}".format(x, self.name))
+	
+	def get_name(self): return self.name
+	def get_info(self): return [self.id, self.name, self.inputType, self.rootName]
 
-
-class investor():
-	def __init__(self, id, name, inputType, rootName = None):
-		self.id = id
+	def __init__(self, _id, name, inputType, rootName = None):
+		self.id = _id
 		self.name = name.title()
 		self.inputType = inputType
 		self.rootName = rootName
 
-		#value is True if the investor is a company
+		#type value is True if the investor is a company
 		self.type = nameAnalysis(name)
+		self.debugInvestor()
 
-		
-	def get_name(self): return self.name
-	def get_info(self): return [self.id, self.name, self.inputType, self.rootName]
-
-	def compare(comparedInvestorName):
-		# compare the investor's name with the existing ones
+	def compare(self, comparedInvestorName):
+		"""compare the investor's name with the existing ones"""
 		# return True if compared investors are the same
 
-		if not self.inputType:
+		if not self.type or comparedInvestorName.split()[0] in REJECTED_SPECIAL_IDENTIFIERS or abs(len(self.name.split()) - len(comparedInvestorName.split())) >= 3 :
 			return False
 
-		if comparedInvestorName.split()[0] in self.name:
-			return True
-
-
-
-
+		for subName in self.name.split():
+			# splitting existing investor name prevent chinese name overlap (Zhengjiang and Zheng)
+			if comparedInvestorName.split()[0] == subName:
+				return True
 
 
 def main():
-	filename = input('Filename input: ')
 	investors = []
 	
-	with open(filename, 'r') as f:
+	with open("output.csv", mode = 'r', encoding = 'utf8') as f:
 		reader = csv.DictReader(f)
 		
+		for num in range(0, 37):
+			reader.__next__()
+
 		for row in reader:
-			for singleInvestor in investors:
-				if singleInvestor.compare(row['name'].title()):
-					investors[index(singleInvestor)] = [singleInvestor, investor(row['id'], row['name'], row['input'], singleInvestor.get_name())]
-				else:
-					if nameAnalysis(row['name']):
-						investors.append(investor(row['id'], row['name'], row['input']))
+			if not investors:
+				# first investor in the list
+				investors.append(Investor(row['id'], row['name'], row['type']))
+			else:
+				isCompared = False
+				for singleInvestor in reversed(investors):
+					if singleInvestor.compare(row['name'].title()):
+						investors.append(Investor(row['id'], row['name'], row['type'], singleInvestor.get_name()))
+						isCompared = True
+						break
+				if not isCompared:
+					investors.append(Investor(row['id'], row['name'], row['type']))
 
 	with open('cleanedInvestor.csv','w', newline = '' ) as f:
 		fieldnames = ['id','name','type','root']
