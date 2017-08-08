abe11f5d5314feef5f2bf43a54c722e4d7eabaa3
nwihardjo <nwihardjo@connect.ust.hk>
2017-08-08 18:10:37 +0800

    add hard coded algo

---
+++ constants.py
@@ -0,0 +1,12 @@
+API_KEY = 'ac5e0a11-8406-4b94-a57f-b1a6661b0886'
+URL = 'https://parserator.datamade.us/api/probablepeople/'
+
+# Investor which has 'University' in it won't be grouped
+
+
+REJECTED_SPECIAL_IDENTIFIERS = ['Zhuhai', 'Yunnan', 'York', 'Wu', 'White', 'University', 'William']
+REJECTED_COMPANY_IDENTIFIERS = ['White Unicorn Ventures']
+
+APPROVED_COMPANY_IDENTIFIERS = ['Tencent', 'tiange.com', 'Xiaomi', 'Innopark', 'Capital', 'International', 
+'Holdings', 'Pharmaceutical', 'Broadcasting', 'Fund', 'Sports', 'Music', 'Inc', 'Angel', 'Ventures',
+'Investment', 'Accelerator', 'Venture', 'Textile', 'Holding','Corporation', 'Sun']
--- main.py
+++ main.py
@@ -1,66 +1,92 @@
 import csv
 import requests
 import json
-import re
+from countryinfo import *
+from constants import *
 
-REJECTED_SPECIAL_IDENTIFIERS = ['Zhuhai', 'Yunnan', 'York', 'Wu','William']
-APPROVED_SPECIAL_IDENTIFIERS = ['Xiaomi']
-API_KEY = 'ac5e0a11-8406-4b94-a57f-b1a6661b0886'
-URL = 'https://parserator.datamade.us/api/probablepeople/'
+def nameRejector(name):
+	"""special name case which for different investors having the same first name"""
+	# return Ture if it is rejected
+	if name.split()[0] in REJECTED_SPECIAL_IDENTIFIERS:
+		return True
+	else:
+		return False
 
 def nameAnalysis(name):
 	"""analyse the name passed using probablepeople API and hardcode"""
 	# because probablepeople library can't be easily installed in our machine
 	# return True if it is a company, return False otherwise
-
-
+	for subName in name.split():
+		if subName in APPROVED_COMPANY_IDENTIFIERS or '.' in subName:
+			return True
+		if nameRejector(subName):
+			return False
+			
 	# for investor name which has non-alphanumeric case
 	# exclude the characters in brackets because it will automatically recognised as Corporation
-	name = ' '.join(name.split('(')[0])
+	if not name.replace(" ","").isalnum():
+		name = ' '.join(name.split('(')[:-1])
 
 	params = {'api_key': API_KEY, 'name': name, 'fmt': 'json'}
 	response = requests.get(URL, params = params)
 
 	if response.status_code != 200:
-		print('DEBUG: API call failed for {}. Check API call in nameAnalysis function'.format(name))
-
-	if name in APPROVED_SPECIAL_IDENTIFIERS:
-		return True
+		# can be deleted for which below line of code is integrated
+		print('DEBUG: API call {} ({}) for {}. Message: {}'.format(response.json()['meta']['status'],response.status_code, name, response.json()['meta']['message']))
+		print('DEBUG: return False instead. Making a "Person"')
+		return False
 
 	try:
 		return (response.json()['type'] == 'Corporation')
 	except:
 		return False
 
-class Investor:
-	
-	def debugInvestor(self):
-		if self.type == True: x = 'Corporation'
-		else: x = 'Person' 
-		
-		if self.rootName:
-			print("DEBUG: CREATING & ADDING {} NAMED {}, ROOT: {}".format(x, self.name, self.rootName))
-		else:
-			print("DEBUG: CREATING & ADDING {} NAMED {}".format(x, self.name))
-	
-	def get_name(self): return self.name
-	def get_info(self): return [self.id, self.name, self.inputType, self.rootName]
+def hardCodeNameComparator(investorName, comparedInvestorName):
+	"""hard codes of the comparison between two investors"""
+	# return True if the compared investors are not the same
 
+	if nameRejector(comparedInvestorName) or investorName in REJECTED_COMPANY_IDENTIFIERS or abs(len(investorName.split()) - len(comparedInvestorName.split())) >= 3:
+		return True
+
+class Investor:
 	def __init__(self, _id, name, inputType, rootName = None):
-		self.id = _id
-		self.name = name.title()
+		self.id = _id	
 		self.inputType = inputType
 		self.rootName = rootName
 
+		#remove 'the' in the naming
+		if name.split()[0].lower() == 'the':
+			self.name = ' '.join(name.split()[1:])
+		else:
+			self.name = name.title()
+
 		#type value is True if the investor is a company
-		self.type = nameAnalysis(name)
-		self.debugInvestor()
+		if rootName is not None: 
+			self.type = True
+		else: 
+			self.type = nameAnalysis(self.name)
+		
+		self.print_status()
+	
+	def print_status(self):
+		if self.type == True: x = 'Corporation'
+		else: x = 'Person' 
+		
+		print("CREATING {}; NAME: {}; ROOT: {}".format(x, self.name, self.rootName))
+
+	def get_name(self): return self.name
+	def get_info(self): return [self.id, self.name, self.inputType, self.rootName]
+
 
 	def compare(self, comparedInvestorName):
+
+
 		"""compare the investor's name with the existing ones"""
 		# return True if compared investors are the same
+		if comparedInvestorName.split()[0].lower() == 'the':
+			comparedInvestorName = ' '.join(comparedInvestorName.split()[1:])
 
-		if not self.type or comparedInvestorName.split()[0] in REJECTED_SPECIAL_IDENTIFIERS or abs(len(self.name.split()) - len(comparedInvestorName.split())) >= 3 :
+		if not self.type or hardCodeNameComparator(self.get_name(), comparedInvestorName):
 			return False
 
 		for subName in self.name.split():
@@ -75,7 +101,7 @@ def main():
 	with open("output.csv", mode = 'r', encoding = 'utf8') as f:
 		reader = csv.DictReader(f)
 		
-		for num in range(0, 37):
+		for num in range(0, 210):
 			reader.__next__()
 
 		for row in reader:
@@ -99,7 +125,6 @@ def main():
 
 		for investor in investors:
 			writer.writerow(investor.get_info())
-
 
 
 
